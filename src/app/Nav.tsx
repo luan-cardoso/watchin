bc1359f2fa7bc5c0a003e1d064b6ce0f968c7ff1
Luan <luan.cardoso0029@gmail.com>
2025-12-11 12:56:24 -0300

    homepage frontend

---
+++ src/app/Nav.tsx
@@ -0,0 +1,49 @@
+import { faHouse, faHeart, faUser } from "@fortawesome/free-regular-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <nav className="h-full flex flex-col items-center pt-12 gap-2">
+      <h1 className="font-extrabold text-2xl pb-6">
+        WATCHIN<span className="text-emerald-600">'</span>
+      </h1>
+      <NavItem icon={faHouse} href="/">
+        Home
+      </NavItem>
+      <NavItem icon={faHeart} href="/">
+        Favoritos
+      </NavItem>
+      <NavItem icon={faMagnifyingGlass} href="/">
+        Buscar
+      </NavItem>
+      <NavItem icon={faUser} href="/">
+        Amigos
+      </NavItem>
+    </nav>
+  );
+}
+
+function NavItem({
+  icon,
+  href,
+  children,
+}: {
+  icon: any;
+  href: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <Link
+      href={href}
+      className="group flex items-center gap-3 px-2 py-1 w-32 hover:text-emerald-600 transition-colors duration-200"
+    >
+      <FontAwesomeIcon
+        icon={icon}
+        className="text-lg group-hover:text-emerald-600 transition-colors duration-200"
+      />
+      <span className="font-light tracking-wider">{children}</span>
+    </Link>
+  );
+}
